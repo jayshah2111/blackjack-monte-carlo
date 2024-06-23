214e4e0e79272e942d62d7de45108c6be47ee058
Jay <shahjay2111@gmail.com>
2024-06-23 14:10:57 +0530

    Created class for plotting

---
+++ classes/plotter.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import matplotlib.style as style
+from typing import Union, List
+
+from betting.utils import *
+style.use('bmh')
+
+class PlotGraph:
+    def __init__(self, user_input: dict) -> None:
+        self.user_input = user_input
