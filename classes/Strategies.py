ee754ca38eb6b27c25835f7e535778871d9a36d4
Jay <shahjay2111@gmail.com>
2024-06-24 13:10:24 +0530

    Defined strategies class

--- classes/Strategies.py
+++ classes/Strategies.py
@@ -13,3 +13,10 @@ strategies_list = ['fixed_bettor', 'percentage_bettor', 'kelly_criterion',
                    'fixed_martingale', 'percentage_martingale', 'fixed_soros',
                    'percentage_soros', 'fixed_fibonacci', 'percentage_fibonacci',
                    'fixed_DAlembert']
+
+class Strategies(ABC):
+    def __init__(self, bet_results: List[List[bool]], user_input: dict, title: str):
+        self.bet_results = bet_results
+        self.user_input: dict = user_input
+        self.title: str = title
+        self.__bet_value = None
