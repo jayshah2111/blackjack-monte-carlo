2669550930288e82a76f7974cdd3173a900cbd56
Jay <shahjay2111@gmail.com>
2024-07-04 08:36:47 +0530

    FixedMartingale bet_value_calculator_non_fixed

--- classes/Strategies.py
+++ classes/Strategies.py
@@ -225,3 +225,11 @@ class FixedMartingale(Strategies):
         if self.bet_value is None: self._Strategies__bet_value = self.user_input['bet_value']
         self._Strategies__bet_value = self.max_min_verify(self._Strategies__bet_value)
         self.initial_bet_value = self._Strategies__bet_value
+        
+    def bet_value_calculator_non_fixed(self):
+        expected_last_result = True if self.inverted else False
+        previous_bet_result = self._Strategies__sample_result[self._Strategies__bet_result_index - 1]
+        if previous_bet_result == expected_last_result and self._Strategies__bet_result_index > 0 and self.current_round < self.round_limit:
+            self._Strategies__bet_value = self.multiplication_factor*self._Strategies__bet_value
+            self._Strategies__bet_value = self.max_min_verify(self._Strategies__bet_value)
+            self.current_round += 1
